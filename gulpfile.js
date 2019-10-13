14997abc3517b19c65b6cade113d556914037e12
katyakaterina <katekonstantino@gmail.com>
2019-10-13 14:51:46 +0300

    modified gulpfile task style

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,30 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gulp = require('gulp');
+var csso = require('gulp-csso');
+var browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
 gulp.task('style', function(){
-    return gulp.src('./style/**/*.scss')
+    return gulp.src('src/style/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-
-    .pipe(gulp.dest('dist/*.css'));
+    .pipe(csso())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
     
 });
-
+// gulp.task('serve', ['style'], function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+//     gulp.watch('src/style/**/*.scss', ['style']);
+//     gulp.watch("*.html").on('change', browserSync.reload);
+// });
+// gulp.task('watch', ['serve']);
 gulp.task('watch', function () {
-    gulp.watch('./style/**/*.scss', ['style']);
+    gulp.watch('src/style/**/*.scss', ['style']);
 });
